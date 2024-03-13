89e089adf3d477a53270a910d292d1a91d8ada58
tuwnguyen <tuwnguyen@gmail.com>
2024-03-13 10:06:49 +0700

    reset to vue latest

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
